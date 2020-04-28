35dafe575c354722833b543c31857d8a61ddf0f0
Henry Liu <henry.liu.texas@gmail.com>
2020-04-28 16:57:41 -0500

    Add obstacle layer with unknown tracking

--- src/explore_node.cpp
+++ src/explore_node.cpp
@@ -9,6 +9,18 @@ int main(int argc, char **argv) {
   tf::TransformListener tf(ros::Duration(10));
   costmap_2d::Costmap2DROS costmap("explore_costmap", tf);
 
+  costmap_2d::Costmap2D* master_map = costmap.getCostmap();
+
+  unsigned int sizeX = master_map->getSizeInCellsX();
+  unsigned int sizeY = master_map->getSizeInCellsY();
+  unsigned int originX = master_map->getOriginX();
+  unsigned int originY = master_map->getOriginY();
+  unsigned int resolution = master_map->getResolution();
+
+  ROS_INFO("size x: %d size y: %d", sizeX, sizeY);
+  ROS_INFO("origin x: %d origin y: %d", originX, originY);
+  ROS_INFO("resolution: %d", resolution);
+
   ros::AsyncSpinner spinner(1);
   spinner.start();
   ros::waitForShutdown();
